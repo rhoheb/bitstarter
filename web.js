3dcb495cf9e7d7ba7bd39db925199fbc6f062559
Rick Hoheb <hoheb@yahoo.com>
2013-07-12 02:58:33 +0000

    Edit web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
+var infile = "index.html";
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(100);
+
+buf.write(fs.readFileSync(infile), "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
