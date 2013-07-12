195b89721060c4fd5cdefaeaf0dff5c4a1e6da72
Rick Hoheb <hoheb@yahoo.com>
2013-07-12 03:12:57 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,12 +3,8 @@ var fs = require('fs');
 var infile = 'index.html';
 var app = express.createServer(express.logger());
 
-var buf = new Buffer(100);
-
-buf.write(fs.readFileSync(infile), "utf-8");
-
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8');
+  response.send('Hello World 3');
 });
 
 var port = process.env.PORT || 5000;
